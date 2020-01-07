351fecad319adba2e1738e30bd914337dadb8e25
AllNamesRTaken <joel.brage@gmail.com>
2020-01-07 12:08:30 +0100

    fixed deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,15 @@
 var gulp = require("gulp");
+var fs = require("fs");
+
+var package = require("./package.json");
+var version = package.version.split(".").map((v) => parseInt(v) || 0);
+
+gulp.task("bump", gulp.parallel(function(cb) {
+    version[2]++;
+    package.version = version.join(".");
+    fs.writeFileSync("./package.json", JSON.stringify(package, null, 2), { encoding: "utf8" });
+    cb();
+}));
 
 gulp.task("package", gulp.series("bump", function() {
     return gulp.src(["./package.json", "./README.md"])
